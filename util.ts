74ccf53d80768e020edece6916d6e317b31fafe0
letier3110 <letier3110@gmail.com>
2024-12-04 11:26:43 +0000

    fix: rewrite ejs file if it is already present

--- util.ts
+++ util.ts
@@ -4,7 +4,7 @@ const oldPath = "./index.html";
 const newPath = "./index.ejs";
 
 try {
-  await copy(oldPath, newPath);
+  await copy(oldPath, newPath, { overwrite: true });
   console.log(`Renamed ${oldPath} to ${newPath}`);
 } catch (error) {
   console.error(`Error renaming file: ${error}`);
